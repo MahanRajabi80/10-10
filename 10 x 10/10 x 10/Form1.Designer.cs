068ec277a7fa60fc29be2be97c36c048451017e1
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-01-22 21:41:43 +0330

    Add files via upload

---
+++ 10 x 10/10 x 10/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _10_x_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblShow = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblShow
+            // 
+            this.lblShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblShow.Location = new System.Drawing.Point(233, 45);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(259, 180);
+            this.lblShow.TabIndex = 0;
+            this.lblShow.Text = "\"\"";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(331, 269);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show 10*10";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lblShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ 10 x 10/10 x 10/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_x_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            lblShow.Text = "";
+            for(int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    lblShow.Text += (i * j).ToString();
+                    if (i * j <= 9)
+                        lblShow.Text += "      ";
+                    else
+                        lblShow.Text += "    ";
+
+                }
+                lblShow.Text += "\n";
+            }
+        }
+    }
+}
